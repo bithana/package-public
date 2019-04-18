fca4fa8bc2931c0af7913a45d434da47663e18aa
Hailang <thehailang@gmail.com>
2019-04-18 14:33:57 +0800

    Expose

--- csv/index.ts
+++ csv/index.ts
@@ -2,49 +2,49 @@ import * as fs from 'fs'
 import * as $csv from 'csv'
 import * as csv_parse from 'csv-parse'
 
-export namespace csv {
-  /**
-   * Parse CSV From String
-   * @param str
-   *
-   * @example
-   * await csv.parse_csv_string(`a,b,c`)
-   * @example
-   * await csv.parse_csv_string(`a|b|c`, { delimiter: '|' })
-   */
-  export async function parse_csv_string(str: string, options: csv_parse.Options = {}) {
-    return new Promise((resolve, reject) => {
-      $csv.parse(str, options, (e, d) => {
-        if (e)
-          reject(e)
-        else
-          resolve(d)
-      })
-    })
-  }
+export const parse_csv_string = parse
 
-  /**
-   * Parse CSV From File
-   * @param filepath
-   *
-   * @example
-   * await csv.parse_csv_file('path/to/file.csv')
-   * @example
-   * await csv.parse_csv_file('path/to/pipe_separated_value.psv', { delimiter: '|' })
-   */
-  export async function parse_csv_file(filepath: string, options: csv_parse.Options = {}) {
-    const parsed = []
-    return new Promise((resolve, reject) => {
-      fs.createReadStream(filepath)
-        .pipe($csv.parse(options))
-        .on('data', function (row) {
-          parsed.push(row)
-        })
-        .on('end', function () {
-          resolve(parsed)
-        })
+/**
+ * Parse CSV From String
+ * @param str
+ *
+ * @example
+ * await csv.parse_csv_string(`a,b,c`)
+ * @example
+ * await csv.parse_csv_string(`a|b|c`, { delimiter: '|' })
+ */
+export async function parse(str: string, options: csv_parse.Options = {}) {
+  return new Promise((resolve, reject) => {
+    $csv.parse(str, options, (e, d) => {
+      if (e)
+        reject(e)
+      else
+        resolve(d)
     })
-  }
+  })
+}
+
+/**
+ * Parse CSV From File
+ * @param filepath
+ *
+ * @example
+ * await csv.parse_csv_file('path/to/file.csv')
+ * @example
+ * await csv.parse_csv_file('path/to/pipe_separated_value.psv', { delimiter: '|' })
+ */
+export async function parse_csv_file(filepath: string, options: csv_parse.Options = {}) {
+  const parsed = []
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filepath)
+      .pipe($csv.parse(options))
+      .on('data', function (row) {
+        parsed.push(row)
+      })
+      .on('end', function () {
+        resolve(parsed)
+      })
+  })
 }
 
 
--- csv/readme.md
+++ csv/readme.md
@@ -6,6 +6,45 @@
 
 ## Getting started
 
+### Parse csv string
+
+```js
+const csv_str =
+`a1,b1,c1
+a2,b2,c2
+a3,b3,c3
+`
+parse_csv_string(csv_str) // Alias of parse
+   .then(console.log) // [ [ 'a1', 'b1', 'c1' ], [ 'a2', 'b2', 'c2' ], [ 'a3', 'b3', 'c3' ] ]
+```
+
+### Parse csv file
+
+**file.csv**
+```csv
+a1,b1,c1
+a2,b2,c2
+a3,b3,c3
+```
+
+```js
+parse_csv_file('path/to/file.scv')
+  .then(console.log) // [ [ 'a1', 'b1', 'c1' ], [ 'a2', 'b2', 'c2' ], [ 'a3', 'b3', 'c3' ] ]
+```
+
+### Use different delimiter other than `,`
+
+```js
+const csv_str =
+`a1|b1|c1
+a2|b2|c2
+a3|b3|c3
+`
+parse_csv_string(csv_str, {delimiter: '|'})
+   .then(console.log) // [ [ 'a1', 'b1', 'c1' ], [ 'a2', 'b2', 'c2' ], [ 'a3', 'b3', 'c3' ] ]
+```
+
+## Development
 ```bash
 npm i # Install.
 npm t # Run test first.
