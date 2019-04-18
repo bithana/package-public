71eb9a34bf401a77ad82f2ce00788a3521875347
Hailang <thehailang@gmail.com>
2019-04-18 12:51:35 +0800

    Auto commit: 2019-04-18_12:51:35

--- canx-tmp/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)',
-  ],
-}
