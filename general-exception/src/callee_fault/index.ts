1cb263fdda9a3437a977ee1e78e6f5106ec4d02e
Hailang <thehailang@gmail.com>
2019-11-30 16:23:19 +0800

    Auto commit: 2019-11-30_16:23:19

--- general-exception/src/callee_fault/index.ts
+++ general-exception/src/callee_fault/index.ts
@@ -1,9 +1,9 @@
-import { E } from '../../index'
+import { E, T_error_opt } from '../../index'
 
 export class Callee_fault extends E {
   constructor(
-    title    = 'Something went wrong inside the callee.',
-    solution = 'The thing you are calling has an internal state problem, Please contact us to fix this.',
+    title: string | T_error_opt = 'Something went wrong inside the callee.',
+    solution                    = 'The thing you are calling has an internal state problem, Please contact us to fix this.',
   ) { super(title, solution) }
 }
 
--- general-exception/src/caller_fault/index.ts
+++ general-exception/src/caller_fault/index.ts
@@ -1,8 +1,8 @@
-import { E } from '../../index'
+import { E, T_error_opt } from '../../index'
 
 export class Caller_fault extends E {
   constructor(
-    title    = 'You likely have an error in you input.',
-    solution = 'If you are confused about this, contact us to fix it.',
+    title: string | T_error_opt = 'You likely have an error in you input.',
+    solution                    = 'If you are confused about this, contact us to fix it.',
   ) { super(title, solution) }
 }
