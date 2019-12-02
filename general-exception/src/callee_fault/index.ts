b46ed8ed0eebcff53c06e7bc62329fd4d3c393c6
Hailang <thehailang@gmail.com>
2019-12-02 09:52:00 +0800

    Auto commit: 2019-12-02_09:52:00

--- general-exception/src/callee_fault/index.ts
+++ general-exception/src/callee_fault/index.ts
@@ -3,7 +3,7 @@ import { E, T_error_opt } from '../../index'
 export class Callee_fault extends E {
   constructor(
     title: string | T_error_opt = 'Something went wrong inside the callee.',
-    solution                    = 'The thing you are calling has an internal state problem, Please contact us to fix this.',
+    solution: string            = 'The thing you are calling has an internal state problem, Please contact us to fix this.',
   ) { super(title, solution) }
 }
 
